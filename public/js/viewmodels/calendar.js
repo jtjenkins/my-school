0f2801010e158ccb74a02adb19335cb376668ace
Jordan Jenkins <jordan@jordanjenkins.com>
2013-05-15 14:07:48 +0000

    removed unneeded files, added calendar for calendar page using the jQuery full calendar plugin

--- public/js/viewmodels/calendar.js
+++ public/js/viewmodels/calendar.js
@@ -1,6 +1,58 @@
 function CalendarViewModel() {
   var self = this;
   self.name = "Calendar";
+  self.events = ko.observableArray([
+              {
+                title: "English - Test Event",
+                start: "2013-05-06T03:15:30Z",
+                end: "2013-05-10T13:15:30Z",
+                url: "#/assignments?id=1",
+                color: "green",
+                textColor: "white"
+              },
+              {
+                title: "History - Test Event 2",
+                start: "2013-05-06T03:15:30Z",
+                end: "2013-05-10T13:15:30Z",
+                url: "#/assignments?id=1",
+                color: "blue",
+                textColor: "white"
+              },
+              {
+                title: "Math - Test Event klsjdflksjdflksjdfklsjdlkfsjdlkfjsdlkfjskldfjsdlk",
+                start: "2013-05-06T03:15:30Z",
+                end: "2013-05-06T06:15:30Z",
+                url: "#/assignments?id=1",
+                color: "yellow",
+                textColor: "black"
+              },
+              {
+                title: "Biology - Test Event klsjdflksjdflksjdfklsjdlkfsjdlkfjsdlkfjskldfjsdlk",
+                start: "2013-05-06T03:15:30Z",
+                end: "2013-05-01T06:15:30Z",
+                url: "#/assignments?id=1",
+                color: "orange",
+                textColor: "black"
+              },
+              {
+                title: "Football - Test Event klsjdflksjdflksjdfklsjdlkfsjdlkfjsdlkfjskldfjsdlk",
+                start: "2013-05-06T03:15:30Z",
+                end: "2013-05-06T06:15:30Z",
+                url: "#/assignments?id=1",
+                color: "red",
+                textColor: "white"
+              }
+            ]);
+
+  $(document).ready(function(){
+    $('#calendarUI').fullCalendar({
+      eventSources: [
+      {
+        events: self.events()
+      }]
+    });
+    console.log("calendar loaded");
+  });
 }
 
 
